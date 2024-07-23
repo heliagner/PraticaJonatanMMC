88a95d7a261a8dd683ba17c43d275560ee89cd30
heliagner <heliagner@hotmail.com>
2024-07-23 18:56:02 -0300

    Ok

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
+let i
 let num1 = 0 , num2 = 0, max = [];
 
 rl.question('Digite o primeiro numero\n', (num1) => {
@@ -13,13 +14,15 @@ rl.question('Digite o primeiro numero\n', (num1) => {
         do{
             if(max % num1 == 0 && max % num2 == 0){
                 console.log('O MMC é '+max);
-                break;
             }
             max++
-        }while(true)
-
-
-
-        rl.close()
-    })
-})
+        }while(true);
+        do{
+            if (num1 % i == 0 && num2 % i ==0){
+                console.log('O MDC é: '+i );
+                break;
+                }
+                i++
+                }while(true);
+            })
+        })
